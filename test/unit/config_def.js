ba0746463f2c926c8b622cefba0840f5a1a87b12
Tommy Xu <xuyijun@gmail.com>
2016-08-31 02:33:59 +0800

    change to package.json. remove test. update package to the latest

--- test/unit/config_def.js
+++
@@ -1,8 +0,0 @@
-
-describe('config.yaml', function() {
-  it('should be parseable', function() {
-    var source = require('fs').readFileSync(__dirname + '/../../conf/default.yaml', 'utf8')
-    require('js-yaml').safeLoad(source)
-  })
-})
-
